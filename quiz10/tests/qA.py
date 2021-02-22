adc608d8c94fe5372afadd1a641cea40c2c44ffd
Peter-Jantsch <75863835+Peter-Jantsch@users.noreply.github.com>
2021-02-22 08:32:59 -0600

    add q10

---
+++ quiz10/tests/qA.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'qA',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(answer_A) == np.ndarray
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
